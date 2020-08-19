ab485d120cfb83a7630379013f993b13fc3c2b17
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-08-19 17:53:23 -0300

    Updates

---
+++ precache-manifest.6a41dfd1650263ea1f7882372d768a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb3eefcf1b8434f1a26509730dead1",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "289a5cd86ec6592f22fa",
+    "url": "/burger-builder/static/css/main.f0c23d34.chunk.css"
+  },
+  {
+    "revision": "c13a6da7adaa2f66fd2b",
+    "url": "/burger-builder/static/js/2.1009bf13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.1009bf13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289a5cd86ec6592f22fa",
+    "url": "/burger-builder/static/js/main.0760695b.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
