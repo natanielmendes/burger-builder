d3963144b52c9dbcf38359879c5c12649fd9a51a
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-09-20 13:50:27 -0300

    Navigating to the Checkout Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -113,6 +113,7 @@ class BurgerBuilder extends Component {
             .catch(error => {
                 this.setState({ loading: false, purchasing: false });
             });
+        this.props.history.push('/checkout');
     }
     
     render() {
