d42ba28c1ca4b2e1cfb0f46513386e7b5c7703b5
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-09-21 15:40:25 -0300

    Implementing Navigation Links

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/burger-builder/" active>Burger Builder</NavigationItem>
-        <NavigationItem link="/burger-builder/">Checkout</NavigationItem>
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
 
