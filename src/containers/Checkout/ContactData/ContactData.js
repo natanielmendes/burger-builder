49ce6222ec08c1985bfb66c5633fecc1ea81916f
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-10-04 23:45:30 -0300

    Working on an Error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -85,6 +85,7 @@ class ContactData extends Component {
                     ]
                 },
                 value: 'fastest',
+                validation: {},
                 valid: true
             },
         },
@@ -117,20 +118,21 @@ class ContactData extends Component {
     checkValidity(value, rules) {
         let isValid = true;
 
-        if (rules) {
-            if (rules.required) {
-                isValid = value.trim() !== '' && isValid;
-            }
-    
-            if (rules.minLength) {
-                isValid = value.length >= rules.minLength && isValid;
-            }
-    
-            if (rules.maxLength) {
-                isValid = value.length <= rules.maxLength && isValid;
-            }
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
         }
 
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
 
         return isValid;
     }
